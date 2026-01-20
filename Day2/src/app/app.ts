a612e052f20206021aa7875a299bc0a9ef5b4b3b
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-20 11:55:45 +0200

    create the basic of the the table component

--- Day2/src/app/app.ts
+++ Day2/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Register } from './components/register/register';
+import {Students} from './components/students/students';
 
 @Component({
   selector: 'app-root',
-  imports: [Register],
+  imports: [Register, Students],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
