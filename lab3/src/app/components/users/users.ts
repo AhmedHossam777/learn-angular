147aa088364d8f42cedb794035ac0a5f911c2457
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-21 01:49:12 +0200

    add users component

--- lab3/src/app/components/users/users.ts
+++ lab3/src/app/components/users/users.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+export type User = {
+  id: number;
+  username: string;
+  email: string;
+  age: number;
+};
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './users.html',
   styleUrl: './users.css',
+  standalone: true,
 })
 export class Users {
-
+  users: User[] = [
+    {
+      id: 1,
+      username: 'ahmed',
+      email: 'ahmed@email.com',
+      age: 24,
+    },
+    {
+      id: 1,
+      username: 'ahmed',
+      email: 'ahmed@email.com',
+      age: 24,
+    },
+    {
+      id: 1,
+      username: 'ahmed',
+      email: 'ahmed@email.com',
+      age: 24,
+    },
+    {
+      id: 1,
+      username: 'ahmed',
+      email: 'ahmed@email.com',
+      age: 24,
+    },
+  ];
 }
