b20509bed6f19e520f938b8b81e44416d5980c5e
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-01-21 10:27:16 +0200

    add user details

--- lab3/src/app/components/user-details/user-details.ts
+++ lab3/src/app/components/user-details/user-details.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-details.html',
   styleUrl: './user-details.css',
 })
 export class UserDetails {
-
+  user = {
+    id: 1,
+    username: 'ahmed',
+    email: 'ahmed@email.com',
+    age: 24,
+  };
 }
